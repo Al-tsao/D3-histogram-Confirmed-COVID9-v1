8519e9be50cc45d93b356be157bea85c7a10171f
AlTsao <al.tsao.yw@gmail.com>
2021-06-19 11:30:33 +0800

    set up x axis

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 async function draw() {
   // Data
-  const dataset = await d3.csv('COVID19.csv')
+  const dataset = await d3.csv('test.csv')
 
   // Data Parse
   const parseData = d3.timeParse('%Y/%m/%d')
   const xAccessor = d => parseData(d.date)
+  const yAccessor = d => parseInt(d.daily_confirmed)
 
   // Dimensions
   let dimensions = {
-    width: 800,
+    width: 600,
     height: 400,
-    margins: 50
+    margins: 50,
+    padding: 1,
   };
 
   dimensions.ctrWidth = dimensions.width - dimensions.margins * 2
@@ -23,12 +25,41 @@ async function draw() {
     .attr("height", dimensions.height)
 
   const ctr = svg.append("g") // <g>
+    .attr("width", dimensions.ctrWidth)
+    .attr("height", dimensions.ctrHeight)
     .attr(
       "transform",
       `translate(${dimensions.margins}, ${dimensions.margins})`
     )
 
+  // Scales
+  const xScale = d3.scaleUtc()
+    .domain(d3.extent(dataset, xAccessor))
+    .range([0, dimensions.ctrWidth])
 
+
+  // Bars
+  ctr.selectAll('rect')
+    .data(dataset)
+    .join('rect')
+    .attr('width', d => {
+      if (xScale(xAccessor(d)) === dimensions.ctrWidth) {
+        return
+      } else {
+        let width = dimensions.ctrWidth / (dataset.length - 1)
+        if (width > 1) {
+          width = dimensions.ctrWidth / (dataset.length - 1) - dimensions.padding
+        }
+        return width
+      }
+    })
+    .attr('x', d => {
+      if (xScale(xAccessor(d)) === dimensions.ctrWidth) {
+        return
+      } else {
+        return xScale(xAccessor(d))
+      }
+    })
 }
 
 draw()
